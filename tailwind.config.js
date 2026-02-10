50ef9c16b859fa53fd6d5cd7473ac7249387ef53
asia-citybeauty <asialevesquegault@gmail.com>
2026-02-10 12:01:14 -0500

    tailwind colours update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'thresh-blue': '#0066FF',
+        'thresh-blue': '#3B36C9',  // Main brand color: rgb(59, 54, 201)
+        'thresh-light': '#D7D7F5',  // Lighter accent color
         'thresh-dark': '#1a1a1a',
       },
     },
